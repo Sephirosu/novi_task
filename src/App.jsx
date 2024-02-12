bf507318dd613537212a5515b48f1a9f1f191c08
Irfan <kal-el88@live.com>
2024-02-12 21:54:43 +0100

    Add all my files

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+function App() {
+  const [circles, setCircles] = useState([]);
+
+  const addCircleHandler = (e) => {
+    const newCircle = {
+      x: e.clientX,
+      y: e.clientY,
+    };
+
+    setCircles((prevCircle) => [...prevCircle, newCircle]);
+  };
+
+  const undoCircles = () => {
+    setCircles(circles.splice(1));
+  };
+
+  //Upitnik :)
+  const redoCircle = () => {};
+
+  return (
+    <div
+      className="relative w-screen h-screen cursor-wait"
+      onClick={addCircleHandler}
+    >
+      {circles.map((circle, index) => (
+        <div
+          className="absolute w-12 h-12 bg-blue-500 rounded-full"
+          style={{
+            left: `${circle.x - 25}px`,
+            top: `${circle.y - 25}px`,
+          }}
+          key={index}
+        ></div>
+      ))}
+      <div className="fixed">
+        <button
+          onClick={redoCircle}
+          className="flex p-5 rounded-lg  bg-green-800 m-10"
+        >
+          Redo
+        </button>
+        <button
+          onClick={undoCircles}
+          className="flex p-5 rounded-lg bg-red-800 m-10"
+        >
+          Undo
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
